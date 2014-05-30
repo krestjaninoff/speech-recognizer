0c99dbfbf538f0071d5bb54d73ae6ab328948894
krestjaninoff <mikhail.krestjaninoff@gmail.com>
2014-05-30 17:59:51 +0400

    Minor fixes

--- src/_test/math/Statistics.cpp
+++ src/_test/math/Statistics.cpp
@@ -11,7 +11,7 @@ TEST(MATH_STATISTICS, RMS)
 	data.push_back(3);
 	data.push_back(1);
 
-	const vector<raw_t>& dataRef = *data;
+	const vector<raw_t>& dataRef = data;
 	double rms = Statistics::rms(dataRef, 0, data.size() - 1);
 
 	EXPECT_TRUE(fabs(rms - 2.7386) < numeric_limits<double>::epsilon());
@@ -33,7 +33,7 @@ TEST(MATH_STATISTICS, ENTROPY)
 	data.push_back(1);
 	data.push_back(7);
 
-	const vector<raw_t>& dataRef = *data;
+	const vector<raw_t>& dataRef = data;
 	double rms = Statistics::entropy(dataRef, 0, data.size() - 1, 4, 0, 10);
 
 	EXPECT_TRUE(fabs(rms - 2.7386) < numeric_limits<double>::epsilon());
