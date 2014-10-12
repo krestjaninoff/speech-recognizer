b8085190e789e646e186483e39f13d4f92ead574
Mikhail Krestjaninoff <mikhail.krestjaninoff@gmail.com>
2014-10-12 22:03:14 +0400

    Baum-Welch tests

--- src/_test/math/BaumWelchTest.cpp
+++ src/_test/math/BaumWelchTest.cpp
@@ -15,6 +15,8 @@ const double HMM_TEST_EPSILON = 1e-2;
 
 static void checkModel(HmModel* model, double** transitionsX, double** emissionsX,
 		double* initialDstX);
+static void cleanUp(HmModel* model, double** transitionsX, double** emissionsX,
+		double* initialDstX);
 
 /**
  * The simplest test
@@ -65,6 +67,111 @@ TEST(BAUM_WELCH, SIMPLE) {
 
 	// Check the results
 	checkModel(model, transitionsX, emissionsX, initialDstX);
+	cleanUp(model, transitionsX, emissionsX, initialDstX);
+}
+
+/**
+ * The simplest test (initial model is optimal)
+ */
+TEST(BAUM_WELCH, SIMPLE_EQUALS) {
+	string text = "test";
+
+	size_t stateCnt = 2;
+	state_t* states = new state_t[2] {'s', 't'};
+
+	size_t observationCnt = 2;
+	observation_t* observations = new observation_t[2] {'A', 'B'};
+
+	double** transitions = new double*[2] {
+		new double[2] {0.0, 1.0},
+		new double[2] {0.5, 0.5}
+	};
+	double** emissions = new double*[2] {
+		new double[2] {1.0, 0.0},
+		new double[2] {0.0, 1.0}
+	};
+	double* initialDst = new double[2] {1.0, 0.0};
+
+	HmModel* model = new HmModel();
+	model->init(states, stateCnt, observations, observationCnt,
+			transitions, emissions, initialDst, text);
+
+	vector<observation_t> sequence;
+	sequence.push_back('A');
+	sequence.push_back('B');
+	sequence.push_back('B');
+	sequence.push_back('A');
+
+	// Model's attributes to check with
+	double** transitionsX = new double*[2] {
+		new double[2] {0.0, 1.0},
+		new double[2] {0.5, 0.5}
+	};
+	double** emissionsX = new double*[2] {
+		new double[2] {1.0, 0.0},
+		new double[2] {0.0, 1.0}
+	};
+	double* initialDstX = new double[2] {1.0, 0.0};
+
+	// Run the algorithm
+	const vector<observation_t>* observationsConst = &sequence;
+	BaumWelch::perform(model, observationsConst);
+
+	// Check the results
+	checkModel(model, transitionsX, emissionsX, initialDstX);
+	cleanUp(model, transitionsX, emissionsX, initialDstX);
+}
+
+/**
+ * The simplest test (initial data is opposite to actual values)
+ */
+TEST(BAUM_WELCH, SIMPLE_REVERSE) {
+	string text = "test";
+
+	size_t stateCnt = 2;
+	state_t* states = new state_t[2] {'s', 't'};
+
+	size_t observationCnt = 2;
+	observation_t* observations = new observation_t[2] {'A', 'B'};
+
+	double** transitions = new double*[2] {
+		new double[2] {1.0, 0.0},
+		new double[2] {0.9, 0.1}
+	};
+	double** emissions = new double*[2] {
+		new double[2] {0.0, 1.0},
+		new double[2] {1.0, 0.0}
+	};
+	double* initialDst = new double[2] {0.0, 1.0};
+
+	HmModel* model = new HmModel();
+	model->init(states, stateCnt, observations, observationCnt,
+			transitions, emissions, initialDst, text);
+
+	vector<observation_t> sequence;
+	sequence.push_back('A');
+	sequence.push_back('B');
+	sequence.push_back('B');
+	sequence.push_back('A');
+
+	// Model's attributes to check with
+	double** transitionsX = new double*[2] {
+		new double[2] {0.0, 1.0},
+		new double[2] {0.5, 0.5}
+	};
+	double** emissionsX = new double*[2] {
+		new double[2] {1.0, 0.0},
+		new double[2] {0.0, 1.0}
+	};
+	double* initialDstX = new double[2] {1.0, 0.0};
+
+	// Run the algorithm
+	const vector<observation_t>* observationsConst = &sequence;
+	BaumWelch::perform(model, observationsConst);
+
+	// Check the results
+	checkModel(model, transitionsX, emissionsX, initialDstX);
+	cleanUp(model, transitionsX, emissionsX, initialDstX);
 }
 
 static void checkModel(HmModel* model, double** transitionsX, double** emissionsX,
@@ -84,4 +191,21 @@ static void checkModel(HmModel* model, double** transitionsX, double** emissions
 	for (size_t i = 0; i < model->getStateCnt(); i++) {
 		EXPECT_NEAR(model->getInitialDst()[i], initialDstX[i], HMM_TEST_EPSILON);
 	}
+}
+
+static void cleanUp(HmModel* model, double** transitionsX, double** emissionsX,
+		double* initialDstX) {
+	delete [] initialDstX;
+
+	for (size_t i = 0; i < model->getStateCnt(); i++) {
+		delete [] transitionsX[i];
+	}
+	delete [] transitionsX;
+
+	for (size_t i = 0; i < model->getStateCnt(); i++) {
+		delete [] emissionsX[i];
+	}
+	delete [] emissionsX;
+
+	delete [] model;
 }
